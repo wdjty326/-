fc0be39f03dc3245af1126dc0d09d60c3ca9bbaf
wdjty326 <wdjty326@gmail.com>
2019-09-27 20:28:06 +0900

    라이브러리 변경

--- src/method/play.js
+++ src/method/play.js
@@ -1,13 +1,15 @@
 
-const ytdl = require("ytdl");
+const ytdl = require("ytdl-core");
+const streamOptions = { seek: 0, volume: 1 };
 
 module.exports = function(message, args) {
-	const broadcast = this.client.createVoiceBroadcast();
 	if (this.connect) {
 		const stream = ytdl(args[0], { filter : 'audioonly' });
+		const broadcast = this.client.createVoiceBroadcast();
 		broadcast.playStream(stream);
-
-		this.connect.playBroadcast(broadcast);
+		const dispatcher = this.connect.playBroadcast(broadcast);
+		dispatcher.setVolumeLogarithmic(100);
+		console.log(dispatcher);
 	} else {
 		message.reply("방부터 들어가자");
 	}
