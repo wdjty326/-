e518cdc200146e9025908c23f8cba9a124d7f36e
wdjty326 <wdjty326@gmail.com>
2019-10-14 19:52:28 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ fs.readFile(path.resolve(__dirname, "..", "client_token"), (err, data) => {
 	if (err) {
 		console.log(err);
 	} else {
-		new DiscordApp(data.toString());	
+		new DiscordApp(
+			data.toString().replace(/\r\n/g, "")
+		);	
 	}
 });
