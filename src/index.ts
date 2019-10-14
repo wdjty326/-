f5a2306c40ac742ba525ecaa229403f1f5e07429
wdjty326 <wdjty326@gmail.com>
2019-10-14 20:13:02 +0900

    unix fs bug

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,14 @@ import path from "path";
 import fs from "fs";
 import DiscordApp from "./app";
 
+
 fs.readFile(path.resolve(__dirname, "..", "client_token"), (err, data) => {
 	if (err) {
 		console.log(err);
 	} else {
+		console.log(data.toString(), "a");
 		new DiscordApp(
-			data.toString().replace(/\r\n/g, "")
+			data.toString().replace(/(\r|\n| )/g, "")
 		);	
 	}
 });
