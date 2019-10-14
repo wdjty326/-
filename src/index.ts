c2774b1ab1f15e1ac359c9784d7269b2e9aa7476
wdjty326 <wdjty326@gmail.com>
2019-10-14 19:48:41 +0900

    fs readFile 함수 변경
    - 사유 ubuntu에서 안됨

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-// const path = require("path");
-// const fs = require("fs");
-
 import path from "path";
 import fs from "fs";
 import DiscordApp from "./app";
 
-const data = fs.readFileSync(path.resolve(__dirname, "..", "client_token"));
-if (data)
-	new DiscordApp(data.toString());
+fs.readFile(path.resolve(__dirname, "..", "client_token"), (err, data) => {
+	if (err) {
+		console.log(err);
+	} else {
+		new DiscordApp(data.toString());	
+	}
+});
