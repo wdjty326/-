b0b14289d7339e6af57ce279eae59f9c6dc6b4fb
정택유 <wdjty326@gmail.com>
2019-10-19 16:56:23 +0900

    korean method

--- src/method/index.ts
+++ src/method/index.ts
@@ -1,23 +1,32 @@
 import ping from "./ping";
 import help from "./help";
+
 import connect from "./connect";
 import disconnect from "./disconnect";
+
 import play from "./play";
+import playlist from "./playlist";
+
 import pause from "./pause";
 import resume from "./resume";
 import skip from "./skip";
 
+import fuck from "./test";
+
+// 명령어 한글 패치
 const method: {
 	[key: string]: Function;
 } = {
-	ping,
-	help,
-	connect,
-	disconnect,
-	play,
-	pause,
-	resume,
-	skip
+	"ㅍ": ping,
+	"ㄷㅇ": help,
+	"ㅇㅈ": connect,
+	"ㅌㅈ": disconnect,
+	"ㄱ": play,
+	"ㅁㄹ": playlist,
+	"ㅉ": pause,
+	"ㄷㅅ": resume,
+	"ㅅㅋ": skip,
+	"ㅌㅇ": fuck
 };
 
 export default method;
